80345677391535cb1a7a1e71429e5a2d7436b8a3
krindioom <47291437+krindioom@users.noreply.github.com>
2024-05-30 20:34:37 +0700

    реализовал передвижение

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
@@ -7,12 +5,41 @@ public class Player : MonoBehaviour
     [Header("Movement")]
     [Min(0)]
     [SerializeField]
-    private float _speed;
+    private float _speed = 0;
 
     private PlayerMovement _movement;
 
     private void Awake()
     {
         _movement = new PlayerMovement(transform, _speed);
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector2 direction = Vector2.zero;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+
+        _movement.Move(direction);
     }
 }
